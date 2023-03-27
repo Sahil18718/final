112792ceec8ec2253445e59cd036af81d35fbc3d
Sahil18718 <119488054+Sahil18718@users.noreply.github.com>
2023-03-27 16:45:58 +0530

    final

---
+++ routes/post.route.js
@@ -0,0 +1,44 @@
+const express=require("express")
+const { PostModel } = require("../models/post.model")
+const postRouter=express.Router()
+
+postRouter.get("/",async(req,res)=>{
+
+    const post=await PostModel.find()
+    res.status(200).send(post)
+})
+
+postRouter.post("/create",async(req,res)=>{
+    const payload=req.body
+    const post=new PostModel(payload)
+    await post.save()
+    res.status(200).send(" post has been created")
+})
+
+postRouter.patch("/update",async(req,res)=>{
+    const{postID}=req.params
+    const payload=req.body
+    try {
+        await PostModel.findByIdAndUpdate({_id:postID},payload)
+        res.status(200).send("Updated")
+    } catch (error) {
+        res.status(400).send({"msg":error.message})
+    }
+    
+})
+
+postRouter.delete("/delete",async(req,res)=>{
+    const {postID}=req.params
+    try {
+        await PostModel.findByIdAndDelete({_id:postID})
+        res.status(200).send({"msg":"deleted"})
+    } catch (error) {
+        res.status(400).send({"msg":error.message})
+        
+    }
+   
+})
+
+module.exports={
+    postRouter
+}
